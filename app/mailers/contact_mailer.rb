07215fe706c9f518ec5f787f165c2998216ffdb2
Joe Timmer <profsmallpine@gmail.com>
2016-09-11 17:42:44 -0600

    Add reply to email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,6 @@
 class ContactMailer < ActionMailer::Base
   def contact_email(contact)
     @contact = contact
-    mail from: 'joe.timmer89@gmail.com', to: 'profsmallpine@gmail.com',
-         subject: "Website contact from #{@contact.name}"
+    mail to: 'profsmallpine@gmail.com', subject: "Website contact from #{@contact.name}"
   end
 end
