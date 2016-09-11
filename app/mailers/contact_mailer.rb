9db4e8209d1ff975ac45a4598538d5bcf466118c
Joe Timmer <profsmallpine@gmail.com>
2016-09-11 17:46:42 -0600

    Inherit from app mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,4 @@
-class ContactMailer < ActionMailer::Base
+class ContactMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
     mail to: 'profsmallpine@gmail.com', subject: "Website contact from #{@contact.name}"
