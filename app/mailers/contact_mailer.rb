547b989458732412bf1c8778871a13547f66727b
Joe Timmer <profsmallpine@gmail.com>
2016-09-11 18:03:11 -0600

    Fix mailto link

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
-    mail to: 'profsmallpine@gmail.com', subject: "Website contact from #{@contact.name}"
+    mail to: 'profsmallpine@gmail.com',
+         subject: "Website contact from #{@contact.name}"
   end
 end
